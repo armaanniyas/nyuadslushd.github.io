d81745a9177244e90385eae0d0c3021cf183f63a
Phil Wee <pkw2010@nyu.edu>
2023-04-19 18:46:54 +0400

    Get template

---
+++ cards.js
@@ -0,0 +1,56 @@
+
+$("#speaker1").click(function() {
+    $("#overlay-speaker1").toggleClass("hidden");
+});
+
+$("#close-speaker1").click(function() {
+    $("#overlay-speaker1").toggleClass("hidden");
+});
+
+$("#speaker2").click(function() {
+    $("#overlay-speaker2").toggleClass("hidden");
+});
+
+$("#close-speaker2").click(function() {
+    $("#overlay-speaker2").toggleClass("hidden");
+});
+
+$("#speaker3").click(function() {
+    $("#overlay-speaker3").toggleClass("hidden");
+});
+
+$("#close-speaker3").click(function() {
+    $("#overlay-speaker3").toggleClass("hidden");
+});
+
+$("#speaker4").click(function() {
+    $("#overlay-speaker4").toggleClass("hidden");
+});
+
+$("#close-speaker4").click(function() {
+    $("#overlay-speaker4").toggleClass("hidden");
+});
+
+$("#speaker5").click(function() {
+    $("#overlay-speaker5").toggleClass("hidden");
+});
+
+$("#close-speaker5").click(function() {
+    $("#overlay-speaker5").toggleClass("hidden");
+});
+
+$("#speaker6").click(function() {
+    $("#overlay-speaker6").toggleClass("hidden");
+});
+
+$("#close-speaker6").click(function() {
+    $("#overlay-speaker6").toggleClass("hidden");
+});
+
+$("#speaker7").click(function() {
+    $("#overlay-speaker7").toggleClass("hidden");
+});
+
+$("#close-speaker7").click(function() {
+    $("#overlay-speaker7").toggleClass("hidden");
+});
